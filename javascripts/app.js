409847b0b790bd6e980db63d5cfeca4758b52539
Christos Bacharakis <cmpahar@moon>
2012-12-11 04:58:07 +0200

    code style changes. comment added as well

--- javascripts/app.js
+++ javascripts/app.js
@@ -40,6 +40,83 @@
 
 })(jQuery, this);
 
+//Search when hittig enter button in text box
+$("#word").keyup(function(event){
+    if(event.keyCode == 13){
+        $("#search").click();
+    }
+});
+
+//function for hitting the search button or enter
+function find(e1){
+    $.ajax({
+        url: 'http://www.openthesaurus.gr/synonyme/search?q='+$(e1).attr("id")+'&format=application/json',
+        dataType: 'jsonp',
+        crossDomain: true,
+        cache: false
+    }).done(function(json) {
+
+            var result = [];
+            getNames(json, "term");
+            var ff = [];
+            var len = result.length;
+            for(var i=0; i<len; i++){
+                var text = result[i];
+                var finaltext = '<a id='+text+' onclick="find(this)">'+text+'</a> , ';
+        ff.push(finaltext);
+    }
+            $('#results').html(ff);
+            document.getElementById('word').value=$(e1).attr("id");
+            function getNames(obj, name) {
+                for (var key in obj) {
+                    if (obj.hasOwnProperty(key)) {
+                        if ("object" == typeof(obj[key])) {
+                            getNames(obj[key], name);
+                        } else if (key == name) {
+                            result.push(obj[key]);
+                        }
+                    }
+                }
+            }
+        });
+};
+
+//function for clicking a synonym from the results list
+function resulta() {
+    $.ajax({
+        url: 'http://www.openthesaurus.gr/synonyme/search?q='+document.getElementById('word').value+'&format=application/json',
+        dataType: 'jsonp',
+        crossDomain: true,
+        cache: false
+    }).done(function(json) {
+            var result = [];
+            getNames(json, "term");
+            var ff = [];
+            var len = result.length;
+            for(var i=0; i<len; i++){
+                var text = result[i];
+                var finaltext = '<a id='+text+' onclick="find(this)">'+text+'</a> , ';
+                ff.push(finaltext);
+            }
+
+            $('#results').html(ff);
+            function getNames(obj, name) {
+                for (var key in obj) {
+                    if (obj.hasOwnProperty(key)) {
+                        if ("object" == typeof(obj[key])) {
+                            getNames(obj[key], name);
+                        } else if (key == name) {
+                            result.push(obj[key]);
+                        }
+                    }
+                }
+            }
+        });
+
+}
+
+
+//Install button
 $(document).ready(function() {
     $("#install_app").click(function() {
         var request = window.navigator.mozApps.install('http://bacharakis.com/projects/openthesaurus.gr/manifest.webapp');
